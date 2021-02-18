1b39a88ad4cb7d194fa7ec25d64a66091b4023e8
Laura Madalosso <madalossolaura@gmail.com>
2021-02-18 11:11:53 -0300

    Adding Prettier

--- src/components/commons/Cover/index.js
+++ src/components/commons/Cover/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import Text from '../../foundations/Text';
-import Masthead from './styles/Masthead';
+import React from "react";
+import Text from "../../foundations/Text";
+import Masthead from "./styles/Masthead";
 
 export default function Cover() {
   return (
     <Masthead>
-      <Text
-        variant="paragraph1"
-        tag="p"
-      >
+      <Text variant="paragraph1" tag="p">
         Laura Madalosso
       </Text>
       <Text>Front-end Developer</Text>
