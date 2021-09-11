71e8eb9bac33a9c0420af854336033380dd7b810
Laura Madalosso <madalossolaura@gmail.com>
2021-09-10 21:18:21 -0300

    fix: esling for app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
